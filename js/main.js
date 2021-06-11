f95735ca03bd26913f66650839fb252f86604b26
Dima <tdm250692@gmail.com>
2021-06-12 01:43:31 +0300

    fix-menu/grid/img

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-(function () {
-	const burger = document.querySelector('.burger');
-	burger.addEventListener('click', () => {
-		burger.classList.toggle('burger_active');
-	});
-}());
+function openMenu(params) {
+    document.getElementById("sidebar").classList.toggle('active');
+    document.getElementById("body").classList.toggle('lock');
+}
